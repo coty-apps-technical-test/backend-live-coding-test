d072c2670e6da6e5daf8c24349f780f1319c3aa6
Damien Soulard <damien.soulard@gmail.com>
2022-12-22 14:49:58 +0100

    chore: add boilerplate for interview

---
+++ src/client.js
@@ -0,0 +1,5 @@
+// Build your client here
+// Run yarn client or npm client to execute this file
+
+const fetch = require("node-fetch")
+
---
+++ src/server.js
@@ -0,0 +1,4 @@
+// Build your server here
+// Run yarn server or npm server to execute this file
+
+// import express from "express"
